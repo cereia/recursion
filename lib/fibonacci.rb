a468b63104a8eb38aab6cb381fa9f63752483a20
cereia <104402370+cereia@users.noreply.github.com>
2024-06-27 18:28:03 -0400

    Create iterative method for Fibonacci sequence

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,1 +1,18 @@
 # frozen_string_literal: true
+
+# iterative fibonacci
+def fibs_iterative(num)
+  fibs = [0, 1]
+  if num.zero?
+    fibs = [fibs[0]]
+  else
+    fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]) while fibs.length < num
+  end
+  fibs
+end
+
+# p fibs_iterative(8)
+# p fibs_iterative(0)
+# p fibs_iterative(1)
+# p fibs_iterative(3)
+# p fibs_iterative(10)
